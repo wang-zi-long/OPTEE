d2c040a7bd88893a3f9d7d08c6a4ef1771a11a36
wang-zi-long <1974650884@qq.com>
2024-04-07 20:03:47 +0800

    add param and res

---
+++ export.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -r /home/neu/Desktop/OPTEE/source/public/kernel_src/kernel/kernel-5.10/drivers/tee /home/neu/Desktop/OPTEE/OPTEE/tee 
+cp -r /home/neu/Desktop/OPTEE/source/public/optee_src/optee/optee_client /home/neu/Desktop/OPTEE/OPTEE/optee_client
+cp -r /home/neu/Desktop/OPTEE/source/public/optee_src/optee/optee_os /home/neu/Desktop/OPTEE/OPTEE/optee_os
